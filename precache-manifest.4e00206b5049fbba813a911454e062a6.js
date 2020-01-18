1be4c98e9ac3f53324a9f7db1ffda0f19b3a03fc
Hugo Voisin <hugov@bam.tech>
2020-01-18 14:04:19 +0100

    Updates

---
+++ precache-manifest.4e00206b5049fbba813a911454e062a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e2acb5bc9bb28f93ef0b2bd447ded8",
+    "url": "/chants-militaires-web/index.html"
+  },
+  {
+    "revision": "20a61a0f622c0f547747",
+    "url": "/chants-militaires-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa4d07770f49ff41e45e",
+    "url": "/chants-militaires-web/static/js/2.4331350c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/chants-militaires-web/static/js/2.4331350c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20a61a0f622c0f547747",
+    "url": "/chants-militaires-web/static/js/main.b276892d.chunk.js"
+  },
+  {
+    "revision": "d1c89c68e66b547d7197",
+    "url": "/chants-militaires-web/static/js/runtime-main.00ead344.js"
+  }
+]);
