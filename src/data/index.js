d37284ebdfe3d95fb070611542ef500ec42e30b1
Hugo Voisin <hugov@bam.tech>
2020-01-18 11:58:37 +0100

    chore(data): add song data

---
+++ src/data/index.js
@@ -0,0 +1,152 @@
+import Charlotte from './Charlotte.json';
+import LeGarsPopol from './LeGarsPopol.json';
+import LesBiffins from './LesBiffins.json';
+import ChevaliersDeLaTableRonde from './ChevaliersDeLaTableRonde.json';
+import LeVinGaulois from './LeVinGaulois.json';
+import LePékinDeBahut from './LePékinDeBahut.json';
+import LesFines from './LesFines.json';
+import CommandosDAfrique from './CommandosDAfrique.json';
+import LesDragonsDeNoailles from './LesDragonsDeNoailles.json';
+import LaMontagne from './LaMontagne.json';
+import QuandJésusChristCréaLaColoniale from './QuandJésusChristCréaLaColoniale.json';
+import Opium from './Opium.json';
+import JAvaisUnCamarade from './JAvaisUnCamarade.json';
+import LaPetitePiste from './LaPetitePiste.json';
+import LEdelweiss from './LEdelweiss.json';
+import LeBoudin from './LeBoudin.json';
+import LeClairon from './LeClairon.json';
+import LesAfricains from './LesAfricains.json';
+import LaPiémontaise from './LaPiémontaise.json';
+import LaBlancheHermine from './LaBlancheHermine.json';
+import LaMort from './LaMort.json';
+import LeChantDesMarais from './LeChantDesMarais.json';
+import LeChantDesPartisans from './LeChantDesPartisans.json';
+import LeGarsPierre from './LeGarsPierre.json';
+import LeVolontaire from './LeVolontaire.json';
+import LesCosaques from './LesCosaques.json';
+import LesPartisansBlancs from './LesPartisansBlancs.json';
+import ÔFrederi from './ÔFrederi.json';
+import PelotDHennebont from './PelotDHennebont.json';
+import PourLaFranceEnDanger from './PourLaFranceEnDanger.json';
+import LeJourLePlusLong from './LeJourLePlusLong.json';
+import LAncien from './LAncien.json';
+import LesLansquenets from './LesLansquenets.json';
+import ÔDouceFrance from './ÔDouceFrance.json';
+import MaFrance from './MaFrance.json';
+import VeillonsAuSalutDeLEmpire from './VeillonsAuSalutDeLEmpire.json';
+import ChansonDeLOignon from './ChansonDeLOignon.json';
+import LesLanciersPolonais from './LesLanciersPolonais.json';
+import FanfanLaTulipe from './FanfanLaTulipe.json';
+import LaMarseillaise from './LaMarseillaise.json';
+import LeChantDuDépart from './LeChantDuDépart.json';
+import ChantDesGirondins from './ChantDesGirondins.json';
+import SambreEtMeuse from './SambreEtMeuse.json';
+import AlsaceLorraine from './AlsaceLorraine.json';
+import LaMadelonDeLaVictoire from './LaMadelonDeLaVictoire.json';
+import LeRêvePasse from './LeRêvePasse.json';
+import LesÉpisMûrs from './LesÉpisMûrs.json';
+import MarcheLorraine from './MarcheLorraine.json';
+import LePinard from './LePinard.json';
+import LaMadelon from './LaMadelon.json';
+import LaStrasbourgeoise from './LaStrasbourgeoise.json';
+import Verdun from './Verdun.json';
+import EnPassantParLaPortière from './EnPassantParLaPortière.json';
+import LeCombatDeDemain from './LeCombatDeDemain.json';
+import LaCavalcade from './LaCavalcade.json';
+import DeboutLesParas from './DeboutLesParas.json';
+import AdieuDuBataillonDeChoc from './AdieuDuBataillonDeChoc.json';
+import LesHussardsDeBercheny from './LesHussardsDeBercheny.json';
+import GloryGloryAlléluia from './GloryGloryAlléluia.json';
+import LesCommandos from './LesCommandos.json';
+import Le_31DuMoisDAoût from './Le_31DuMoisDAoût.json';
+import SariMarès from './SariMarès.json';
+import LaPrière from './LaPrière.json';
+import GénéralLoustaunauLacau from './GénéralLoustaunauLacau.json';
+import GénéralFourcade from './GénéralFourcade.json';
+import CompagnonsDeLaLibération from './CompagnonsDeLaLibération.json';
+import LesOfficiers from './LesOfficiers.json';
+import LaGalette from './LaGalette.json';
+import LesCasos from './LesCasos.json';
+import NouveauBahut from './NouveauBahut.json';
+import KyrieDesGueux from './KyrieDesGueux.json';
+import BéretRouge from './BéretRouge.json';
+import LesOiesSauvages from './LesOiesSauvages.json';
+
+export default [
+  Charlotte,
+  LeGarsPopol,
+  LesBiffins,
+  ChevaliersDeLaTableRonde,
+  LeVinGaulois,
+  LePékinDeBahut,
+  LesFines,
+  CommandosDAfrique,
+  LesDragonsDeNoailles,
+  LaMontagne,
+  QuandJésusChristCréaLaColoniale,
+  Opium,
+  JAvaisUnCamarade,
+  LaPetitePiste,
+  LEdelweiss,
+  LeBoudin,
+  LeClairon,
+  LesAfricains,
+  LaPiémontaise,
+  LaBlancheHermine,
+  LaMort,
+  LeChantDesMarais,
+  LeChantDesPartisans,
+  LeGarsPierre,
+  LeVolontaire,
+  LesCosaques,
+  LesPartisansBlancs,
+  ÔFrederi,
+  PelotDHennebont,
+  PourLaFranceEnDanger,
+  LeJourLePlusLong,
+  LAncien,
+  LesLansquenets,
+  ÔDouceFrance,
+  MaFrance,
+  VeillonsAuSalutDeLEmpire,
+  ChansonDeLOignon,
+  LesLanciersPolonais,
+  FanfanLaTulipe,
+  LaMarseillaise,
+  LeChantDuDépart,
+  ChantDesGirondins,
+  SambreEtMeuse,
+  AlsaceLorraine,
+  LaMadelonDeLaVictoire,
+  LeRêvePasse,
+  LesÉpisMûrs,
+  MarcheLorraine,
+  LePinard,
+  LaMadelon,
+  LaStrasbourgeoise,
+  Verdun,
+  EnPassantParLaPortière,
+  LeCombatDeDemain,
+  LaCavalcade,
+  DeboutLesParas,
+  AdieuDuBataillonDeChoc,
+  LesHussardsDeBercheny,
+  GloryGloryAlléluia,
+  LesCommandos,
+  Le_31DuMoisDAoût,
+  SariMarès,
+  LaPrière,
+  GénéralLoustaunauLacau,
+  GénéralFourcade,
+  CompagnonsDeLaLibération,
+  LesOfficiers,
+  LaGalette,
+  LesCasos,
+  NouveauBahut,
+  KyrieDesGueux,
+  BéretRouge,
+  LesOiesSauvages,
+].reduce((res, item) => {
+  res[item.id] = item;
+  return res;
+}, {});
