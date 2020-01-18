34940342b5aba9515fb1fdae078a5e86d87315cb
Hugo Voisin <hugov@bam.tech>
2020-01-18 13:15:33 +0100

    chore: remove history as its unused

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Route, Switch, Router } from "react-router-dom";
-import { MusicList } from "./pages/MusicList";
-import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { MusicList, MusicView } from "./pages";
 
 function App() {
   return (
-    <Router history={history}>
+    <Router>
       <Switch>
         <Route exact path={"/"} component={MusicList} />
       </Switch>
