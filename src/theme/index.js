dd1e36912a3ad8ade1a20f22f3338c88a1b9df55
Hugo Voisin <hugov@bam.tech>
2020-01-18 12:02:19 +0100

    feat(theme): add generic theme for styles

---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+const BASE_SPACING = 8;
+
+export default {
+  color: {
+    background: '#272727',
+    header: '#333333',
+    white: '#FFFFFF',
+  },
+  margin: {
+    x1: BASE_SPACING,
+    x2: BASE_SPACING * 2,
+  },
+  fontSize: {
+    title: 24,
+    heading: 20,
+    body: 16,
+  },
+};
