d956a95e49fb2e8efa0f468f6304f6b120dabd95
Hugo Voisin <hugov@bam.tech>
2020-01-18 12:00:50 +0100

    feat(navigation): add router for navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, Router } from "react-router-dom";
+import { MusicList } from "./pages/MusicList";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path={"/"} component={MusicList} />
+      </Switch>
+    </Router>
   );
 }
 
